9d587f284ad91f2896daab29f1d2fd026941425d
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-04-13 15:55:10 +0200

    work on header with scss

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="myContainer text-black">
+        <div class="logo-container">
+            <img src="../assets/img/dc-logo.png" alt="LogoSmall">
+        </div>
+        <div class="navbar">
+            <ul v-for="(item,i) in listHeader"  > 
+                <li class="text-uppercase">
+                    <a href="item.url">{{ item.text }}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+        export default {
+    name: 'HeaderDC',
+    data() {
+        return {
+            listHeader: [
+                {
+                    text: 'characters',
+                    url: '#'
+                },
+                {
+                    text: 'comics',
+                    url: '#'
+                },
+                {
+                    text: 'movies',
+                    url: '#'
+                },
+                {
+                    text: 'tv',
+                    url: '#'
+                },
+                {
+                    text: 'games',
+                    url: '#'
+                },
+                {
+                    text: 'collectibles',
+                    url: '#'
+                },
+                {
+                    text: 'videos',
+                    url: '#'
+                },
+                {
+                    text: 'fans',
+                    url: '#'
+                },
+                {
+                    text: 'news',
+                    url: '#'
+                },
+                {
+                    text: 'shop',
+                    url: '#'
+                }
+            ]
+        }
+    }
+}
+    
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+
+import 'bootstrap/scss/bootstrap.scss';
+
+import './assets/general.scss';
 
 createApp(App).mount('#app')
